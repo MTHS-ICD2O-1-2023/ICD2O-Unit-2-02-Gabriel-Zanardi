2019cd92e242df02db77de005be43dfffd72159a
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-03-08 19:24:31 +0000

    edited a lot of stuff

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function doMathClicked() {
+  document.getElementById("area-rectangle").innerHTML = "<p> 5+3" + (5 + 3) + cm**2 +"</p>"
+  document.getElementById("perimeter-rectangle").innerHTML = "<p> 2(5+3) " + (6 + 5) + cm + "</p>"
+}
